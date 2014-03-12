4d9600b123ced36ba05e7c0eb2ce35ee8455ce75
Tara Nelson & Jeremy Clemans <student@epicodus.com>
2014-03-12 09:38:40 -0700

    5 specs passing

--- lib/term.rb
+++ lib/term.rb
@@ -1,8 +1,31 @@
 class Term
   attr_reader :term, :definition
+  @@all_terms = []
+
+  def Term.clear
+    @@all_terms = []
+  end
+
+  def Term.all
+    @@all_terms
+  end
 
   def initialize(term, definition)
     @term = term
     @definition = definition
+  end
+
+  def Term.create(term, definition)
+    new_term = Term.new(term, definition)
+    @@all_terms << new_term
+    new_term
+  end
+
+  def edit_definition(new_definition)
+    @definition = new_definition
+  end
+
+  def delete
+    @@all_terms.delete_if {|term| term == self}
   end
 end
--- spec/term_spec.rb
+++ spec/term_spec.rb
@@ -1,11 +1,45 @@
 require 'rspec'
 require 'term'
 
-describe 'Term'
+describe 'Term' do
+  before do
+    Term.clear
+  end
+
   describe 'initialize' do
     it 'initializes a Term with default properties' do
       new_term = Term.new('carrot', 'A delicious vegetable.')
       new_term.should be_an_instance_of Term
+    end
+  end
+  describe '.create' do
+    it 'creates an instance of Term and pushes/saves it to an array of all terms' do
+      new_term = Term.create('carrot', 'A delicious vegetable.')
+      Term.all.should eq [new_term]
+    end
+  end
+
+  describe 'edit_definition' do
+    it 'allows definitions to be edited' do
+      new_term = Term.create('plums', 'A delictions fruit')
+      new_term.edit_definition('A delicious fruit')
+      new_term.definition.should eq 'A delicious fruit'
+    end
+  end
+
+  describe '.clear' do
+    it 'clears all Terms from the @@all_terms array' do
+      Term.clear
+      Term.all.should eq []
+    end
+  end
+
+  describe 'delete' do
+    it 'deletes a Term' do
+      new_term = Term.create('bacon', 'The candy of meats')
+      new_term2 = Term.create('apple', 'My favorite fruit')
+      new_term.delete
+      Term.all.should eq [new_term2]
     end
   end
 end
